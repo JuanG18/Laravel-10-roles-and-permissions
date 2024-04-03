4346205d6c2cba39b262f15b4230eb767823a7c3
Juan David Grueso Cuero <116647053+JuanG18@users.noreply.github.com>
2024-04-03 15:02:43 -0500

    errores solucionado
    yupiii

--- app/Http/Controllers/FormularioController.php
+++ app/Http/Controllers/FormularioController.php
@@ -13,17 +13,23 @@ class FormularioController extends Controller
         return view('formularios.index', compact('formularios'));
     }
 
+
     public function create()
     {
         return view('formularios.create');
     }
 
     public function edit(Formulario $formulario)
-    {
-        return view('formularios.edit', compact('formulario'));
-    }
+{
 
-    public function store(Request $request)
+    return view('formularios.edit', [
+        'formulario' => $formulario
+    ]);
+
+}
+
+
+    public function store(Request $request )
     {
         // Validar los datos del formulario
         $request->validate([
@@ -36,11 +42,13 @@ class FormularioController extends Controller
             'rpo_days' => 'required|integer',
             'rpo_hours' => 'required|integer',
             'rpo_minutes' => 'required|integer',
-            'recurso_humano' => 'required', // Agregar validación para recurso humano
-            'herramientas' => 'required', // Agregar validación para herramientas
-            'registros_vitales' => 'required', // Agregar validación para registros vitales
-            'recomendaciones_recuperacion' => 'required', // Agregar validación para recomendaciones de recuperación
+            'recurso_humano' => 'required',
+            'herramientas' => 'required',
+            'registros_vitales' => 'required',
+            'recomendaciones_recuperacion' => 'required',
+
         ]);
+
 
         // Calcula los pesos
         $peso_operativo = $request->nivel_operativo === 'Alta' ? 5 : ($request->nivel_operativo === 'Medio' ? 3 : 1);
@@ -83,72 +91,79 @@ class FormularioController extends Controller
             'herramientas' => $request->herramientas,
             'registros_vitales' => $request->registros_vitales,
             'recomendaciones_recuperacion' => $request->recomendaciones_recuperacion,
+
+
         ]);
 
         return redirect()->back()->with('success', 'Formulario guardado exitosamente.');
     }
 
-    public function update(Request $request, Formulario $formulario)
-    {
-        // Validar los datos recibidos del formulario
-        $request->validate([
-            'nivel_operativo' => 'required|in:Alta,Medio,Bajo',
-            'nivel_financiero' => 'required|in:Alta,Medio,Bajo',
-            'nivel_legal' => 'required|in:Alta,Medio,Bajo',
-            'rto_days' => 'required|integer',
-            'rto_hours' => 'required|integer',
-            'rto_minutes' => 'required|integer',
-            'rpo_days' => 'required|integer',
-            'rpo_hours' => 'required|integer',
-            'rpo_minutes' => 'required|integer',
-            'recurso_humano' => 'required', // Agregar validación para recurso humano
-            'herramientas' => 'required', // Agregar validación para herramientas
-            'registros_vitales' => 'required', // Agregar validación para registros vitales
-            'recomendaciones_recuperacion' => 'required', // Agregar validación para recomendaciones de recuperación
-        ]);
+    public function update(Request $request, Formulario $id)
+{
 
-        // Calcula los pesos
-        $peso_operativo = $request->nivel_operativo === 'Alta' ? 5 : ($request->nivel_operativo === 'Medio' ? 3 : 1);
-        $peso_financiero = $request->nivel_financiero === 'Alta' ? 5 : ($request->nivel_financiero === 'Medio' ? 3 : 1);
-        $peso_legal = $request->nivel_legal === 'Alta' ? 5 : ($request->nivel_legal === 'Medio' ? 3 : 1);
 
-        // Calcula el valor inherente
-        $valor_inherente = ($peso_operativo * 0.5) + ($peso_financiero * 0.2) + ($peso_legal * 0.3);
 
-        // Determina la escala
-        if ($valor_inherente >= 4.1) {
-            $escala = "Crítico";
-        } elseif ($valor_inherente >= 3.1) {
-            $escala = "Alto";
-        } elseif ($valor_inherente >= 2.1) {
-            $escala = "Medio";
-        } elseif ($valor_inherente > 0) {
-            $escala = "Bajo";
-        }
+    $request->validate([
+        'nivel_operativo' => 'required|in:Alta,Medio,Bajo',
+        'nivel_financiero' => 'required|in:Alta,Medio,Bajo',
+        'nivel_legal' => 'required|in:Alta,Medio,Bajo',
+        'rto_days' => 'required|integer',
+        'rto_hours' => 'required|integer',
+        'rto_minutes' => 'required|integer',
+        'rpo_days' => 'required|integer',
+        'rpo_hours' => 'required|integer',
+        'rpo_minutes' => 'required|integer',
+        'recurso_humano' => 'required',
+        'herramientas' => 'required',
+        'registros_vitales' => 'required',
+        'recomendaciones_recuperacion' => 'required',
+    ]);
 
-        // Actualizar los datos del formulario con los datos recibidos del formulario
-        $formulario->update([
-            'nivel_operativo' => $request->nivel_operativo,
-            'nivel_financiero' => $request->nivel_financiero,
-            'nivel_legal' => $request->nivel_legal,
-            'peso_operativo' => $peso_operativo,
-            'peso_financiero' => $peso_financiero,
-            'peso_legal' => $peso_legal,
-            'valor_inherente' => $valor_inherente,
-            'escala' => $escala,
-            'rto_days' => $request->rto_days,
-            'rto_hours' => $request->rto_hours,
-            'rto_minutes' => $request->rto_minutes,
-            'rpo_days' => $request->rpo_days,
-            'rpo_hours' => $request->rpo_hours,
-            'rpo_minutes' => $request->rpo_minutes,
-            'recurso_humano' => $request->recurso_humano,
-            'herramientas' => $request->herramientas,
-            'registros_vitales' => $request->registros_vitales,
-            'recomendaciones_recuperacion' => $request->recomendaciones_recuperacion,
-        ]);
+    // Calcula los pesos
+    $peso_operativo = $request->nivel_operativo === 'Alta' ? 5 : ($request->nivel_operativo === 'Medio' ? 3 : 1);
+    $peso_financiero = $request->nivel_financiero === 'Alta' ? 5 : ($request->nivel_financiero === 'Medio' ? 3 : 1);
+    $peso_legal = $request->nivel_legal === 'Alta' ? 5 : ($request->nivel_legal === 'Medio' ? 3 : 1);
 
-        // Redirigir a alguna vista o ruta después de la actualización exitosa
-        return redirect()->route('formularios.index')->with('success', 'Formulario actualizado exitosamente.');
+    // Calcula el valor inherente
+    $valor_inherente = ($peso_operativo * 0.5) + ($peso_financiero * 0.2) + ($peso_legal * 0.3);
+
+    // Determina la escala
+    if ($valor_inherente >= 4.1) {
+        $escala = "Crítico";
+    } elseif ($valor_inherente >= 3.1) {
+        $escala = "Alto";
+    } elseif ($valor_inherente >= 2.1) {
+        $escala = "Medio";
+    } elseif ($valor_inherente > 0) {
+        $escala = "Bajo";
     }
+
+    // Actualizar los datos del formulario con los datos recibidos del formulario
+    $id->update([
+    'actividad' => $request->actividad,
+    'nivel_operativo' => $request->nivel_operativo,
+    'nivel_financiero' => $request->nivel_financiero,
+    'nivel_legal' => $request->nivel_legal,
+    'peso_operativo' => $peso_operativo,
+    'peso_financiero' => $peso_financiero,
+    'peso_legal' => $peso_legal,
+    'valor_inherente' => $valor_inherente,
+    'escala' => $escala,
+    'rto_days' => $request->rto_days,
+    'rto_hours' => $request->rto_hours,
+    'rto_minutes' => $request->rto_minutes,
+    'rpo_days' => $request->rpo_days,
+    'rpo_hours' => $request->rpo_hours,
+    'rpo_minutes' => $request->rpo_minutes,
+    'recurso_humano' => $request->recurso_humano,
+    'herramientas' => $request->herramientas,
+    'registros_vitales' => $request->registros_vitales,
+    'recomendaciones_recuperacion' => $request->recomendaciones_recuperacion,
+]);
+
+    // Redirigir a alguna vista o ruta después de la actualización exitosa
+    return redirect()->route('formularios.index')
+                     ->with('success', 'Formulario actualizado exitosamente.');
+}
+
 }
